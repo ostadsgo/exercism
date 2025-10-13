45e97d4fb0ce7937f508255bfa652d90a049fe67
Saeed Gholami <saeed.ghollami@gmail.com>
2025-10-14 00:17:04 +0330

    armstrong number.

--- armstrong-numbers/armstrong_numbers.py
+++ armstrong-numbers/armstrong_numbers.py
@@ -1,2 +1,31 @@
+# def is_armstrong_number(number):
+#     str_number = str(number)
+#     len_number = len(str_number)
+#     result_number = 0
+#
+#     for digit in str_number:
+#         result_number += int(digit) ** len_number
+#
+#     return number == result_number
+
+
+
+
+# def is_armstrong_number(number):
+#     digits = [int(n) for n in str(number)]
+#     res = [d ** len(digits) for d in digits]
+#     return sum(res) == number
+
 def is_armstrong_number(number):
-    pass
+    return sum(int(digit) ** len(str(number)) for digit in str(number)) == number
+#
+
+# def is_armstrong_number(number):
+#     orginal_number = number
+#     res = 0
+#     n = len(str(number))
+#     while number > 0:
+#         number, digit = divmod(number, 10)
+#         res += digit ** n
+#
+#     return res == orginal_number
